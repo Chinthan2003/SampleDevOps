2830afe9d332d4c1b1c2fc99ea8d8ac74e5d58c9
RManojPawar <rmanojpawar2004@gmail.com>
2025-03-14 10:10:05 +0530

    First Commit

--- SampleJava/src/LoginTest.java
+++ SampleJava/src/LoginTest.java
@@ -1,2 +1,18 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        // user-name password login-button
+        driver.findElement(By.id("user-name")).
+                sendKeys( "standard_user");
+        driver.findElement(By.id("password")).
+                sendKeys( "secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
 }
